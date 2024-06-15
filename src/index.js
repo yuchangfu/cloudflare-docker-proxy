b2922dfb65f6ea2fea0fb389e7e9f3897337ff2e
yuchangfu <yuchangfu@foxmail.com>
2024-06-15 18:24:28 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.yuchangfu.com": dockerHub,
+  "quay.yuchangfu.com": "https://quay.io",
+  "gcr.yuchangfu.com": "https://gcr.io",
+  "k8s-gcr.yuchangfu.com": "https://k8s.gcr.io",
+  "k8s.yuchangfu.com": "https://registry.k8s.io",
+  "ghcr.yuchangfu.com": "https://ghcr.io",
+  "cloudsmith.yuchangfu.com": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
